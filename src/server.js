8b8d479f2843ba751cbba4abf420cd6b1da507b3
Mythili <mythili4997@gmail.com>
2025-12-10 07:47:38 +0530

    added testRuote

--- src/server.js
+++ src/server.js
@@ -11,3 +11,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+app.get("/checkdb", async (req, res) => {
+  const [rows] = await pool.query("SELECT DATABASE()");
+  res.json(rows);
+});
